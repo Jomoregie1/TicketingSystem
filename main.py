dfa594c6d54059cea2d4d8c41843ebfeed75f57a
Joseph Omoregie <76253926+Jomoregie1@users.noreply.github.com>
2024-04-03 09:52:45 +0100

    Update main.py
    Refactored main file

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from user import User
 from admin import Admin
 
-while True:
-    type_of_user = input("Are you an Admin or User?(Admin/User) ").lower()
-    if type_of_user == "admin":
-        Admin()
-        if Admin.isTerminated:
-            Admin.isTerminated = False
-            break
-    elif type_of_user == "user":
-        User()
-    else:
-        print("Try again, Invalid input")
+
+def run(): 
+    
+    while True:
+        type_of_user = input("Are you an Admin or User?(Admin/User) ").lower()
+        if type_of_user == "admin":
+            Admin()
+            if Admin.isTerminated:
+                Admin.isTerminated = False
+                break
+        elif type_of_user == "user":
+            User()
+        else:
+            print("Try again, Invalid input")
+
+
+
+
+if __name__ == "__main__":
+    run() 
